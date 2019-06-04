b55db204d40398926da2697c78487bfb44329409
Diego O <diosuna@redelcid.com>
2019-06-04 16:24:07 -0600

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app" class="">
+    <div v-if="$router.history.current.path !=='/login'" id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> | 
+      <router-link to="/login">Login</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.btn-accent{
+  background-color: #448aff;
+  color: #fff;
+}
+.btn-accent:hover {
+  background-color: #376ac2;
+  color: #fff !important;
+}
+
+.centered {
+    margin: 0 auto;
+  }
+
+.logo{
+  width: 250px;
+  max-width: 300px;
+  height: auto;
+  margin: 10px;
+}
+
+.bg-yellow {
+  background-color:#FF9800;
+}
+
+a {
+  text-decoration: underline !important;
+}
+
+a:hover {
+  text-decoration: underline !important;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="login">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-10 col-md-8 col-lg-6 centered">
+          <img class="logo" alt="Parken" src="../assets/logo.png">
+          <div class="card shadow ">
+            <article class="card-body">
+              <h4 class="card-title text-center mb-4 mt-1">Inicio de Sesión</h4>
+              <hr>
+              <p class="text-center">Por favor introduzca su nombre de usuario y contraseña</p>
+              <form>
+                <div class="form-group">
+                  <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+                    </div>
+                    <input name="" class="form-control" placeholder="Usuario" type="email">
+                  </div> <!-- input-group.// -->
+                </div> <!-- form-group// -->
+                <div class="form-group">
+                  <div class="input-group">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+                    </div>
+                      <input class="form-control" placeholder="******" type="password">
+                  </div> <!-- input-group.// -->
+                </div> <!-- form-group// -->
+                <div class="form-group">
+                  <button type="submit" class="btn btn-accent btn-block">Iniciar Sesión</button>
+                </div> <!-- form-group// -->
+                <p class="text-center"><a href="#" class="btn">¿Olvidó su contraseña?</a></p>
+              </form>
+            </article>
+          </div> <!-- card.// -->
+        </div>
+      </div>      
+    </div> 
+  </div>
+</template>
+
+<style>
+  
+
+  .login {
+    height: 100vh;
+    background: linear-gradient(#FF9800 330px, #fff 330px);
+  }
+
+  .container{
+    padding: 30px;    
+  }
+
+  .card {
+    margin-top: 25px;
+  }
+</style>
